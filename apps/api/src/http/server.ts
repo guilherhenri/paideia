d664b93d4f67a4ccb4cca323e3b15e2029ee1952
Guilherme Henrique <guilherhenri12@gmail.com>
2025-05-12 07:58:07 -0300

    feature: create project e configure zod with swagger

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,42 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { env } from '../env'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, { origin: '*' })
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Paideia API',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
