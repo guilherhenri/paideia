99fc81b8305ecb6313de81d36e2994bf7904df1b
Guilherme Henrique <guilherhenri12@gmail.com>
2025-05-19 09:11:57 -0300

    feature: add and configure jwt/cookie

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,6 @@
+import { fastifyCookie } from '@fastify/cookie'
 import { fastifyCors } from '@fastify/cors'
+import { fastifyJwt } from '@fastify/jwt'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
@@ -11,12 +13,40 @@ import {
 
 import { env } from '@/env'
 
+import { errorHandler } from './error-handler'
 import { routes } from './routes'
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: env.REFRESH_TOKEN_NAME,
+    signed: false,
+  },
+  verify: {
+    extractToken: (request) => {
+      if (request.url === '/token/refresh') {
+        return request.cookies && request.cookies[env.REFRESH_TOKEN_NAME]
+      } else {
+        return (
+          request.headers.authorization &&
+          request.headers.authorization.split(' ')[1]
+        )
+      }
+    },
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
+
+app.setErrorHandler(errorHandler)
 
 app.register(fastifyCors, { origin: '*' })
 
