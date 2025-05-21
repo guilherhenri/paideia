2d9aa212bb4b50cf5ca38d9fc293eb9a7406f973
Guilherme Henrique <guilherhenri12@gmail.com>
2025-05-21 13:57:18 -0300

    feature: authenticate with password route

--- apps/api/src/http/routes/index.ts
+++ apps/api/src/http/routes/index.ts
@@ -1,7 +1,9 @@
 import type { FastifyTypedInstance } from '@/types'
 
+import { authRoutes } from './auth'
 import { userRoutes } from './user'
 
 export async function routes(app: FastifyTypedInstance) {
+  app.register(authRoutes)
   app.register(userRoutes)
 }
