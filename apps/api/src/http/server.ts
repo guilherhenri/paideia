ca73725c23dce9996ef28de320179bd3f1121a68
Guilherme Henrique <guilherhenri12@gmail.com>
2025-05-21 08:18:41 -0300

    chore: add authentication to swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -3,6 +3,7 @@ import { fastifyCors } from '@fastify/cors'
 import { fastifyJwt } from '@fastify/jwt'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { env } from '@paideia/env'
 import { fastify } from 'fastify'
 import {
   jsonSchemaTransform,
@@ -10,8 +11,6 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-
-import { env } from '@/env'
 
 import { errorHandler } from './error-handler'
 import { routes } from './routes'
@@ -24,13 +23,13 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   cookie: {
-    cookieName: env.REFRESH_TOKEN_NAME,
+    cookieName: env.REFRESH_TOKEN_COOKIE_NAME,
     signed: false,
   },
   verify: {
     extractToken: (request) => {
       if (request.url === '/token/refresh') {
-        return request.cookies && request.cookies[env.REFRESH_TOKEN_NAME]
+        return request.cookies && request.cookies[env.REFRESH_TOKEN_COOKIE_NAME]
       } else {
         return (
           request.headers.authorization &&
@@ -51,10 +50,18 @@ app.setErrorHandler(errorHandler)
 app.register(fastifyCors, { origin: '*' })
 
 app.register(fastifySwagger, {
-  openapi: {
+  swagger: {
     info: {
       title: 'Paideia API',
       version: '1.0.0',
+    },
+    securityDefinitions: {
+      Authorization: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: 'JWT obtained from authentication route.',
+      },
     },
   },
   transform: jsonSchemaTransform,
